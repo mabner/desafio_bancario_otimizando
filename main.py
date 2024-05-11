f7ab772993aa1e355c92d0c275397d8125cc5d26
Marcos Leite <m.abner@live.co.uk>
2024-05-11 01:56:46 -0300

    minor adjustments to string sentences

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ menu = """
 
 => """
 
-balance = 0
+balance = 0.00
 limit = 500
 bank_statement = ""
 withdraw_count = 0
@@ -18,14 +18,16 @@ while True:
     option = input(menu)
 
     if option == "d":
-        amount = float(input("Insert the amount do deposit: "))
+        amount = float(input("Insert the amount to deposit: "))
 
         if amount > 0:
             balance += amount
+
+            # adds transaction to bank statement
             bank_statement += f"Deposit: R$ {amount:.2f}\n"
 
         else:
-            print("The operation failed! The informed amount is invalid.")
+            print("Operation failed! Invalid amount.")
 
     elif option == "w":
         amount = float(input("Inform the amount to withdraw: "))
@@ -37,30 +39,33 @@ while True:
         exceeded_withdrawals = withdraw_count >= WITHDRAW_COUNT_LIMIT
 
         if exceeded_balance:
-            print("The operation failed! You don't have enough balance.")
+            print("Operation failed! Not enough balance.")
 
         elif exceeded_limit:
-            print("The operation failed! The withdraw amount exceeds the limit.")
+            print("Operation failed! Withdraw amount exceeds the limit.")
 
         elif exceeded_withdrawals:
-            print("The operation failed! Number of daily withdrawals exceeded.")
+            print("Operation failed! Daily withdrawals exceeded.")
 
         elif amount > 0:
             balance -= amount
+
+            # adds transaction to bank statement
             bank_statement += f"Withdraw: R$ {amount:.2f}\n"
+
             withdraw_count += 1
 
         else:
-            print("The operation failed! The informed amount is invalid.")
+            print("Operation failed! Invalid amount.")
 
     elif option == "s":
         print("\n================ BANK STATEMENT ================")
-        print("No transactions." if not bank_statement else bank_statement)
+        print("No transactions found." if not bank_statement else bank_statement)
         print(f"\nBalance: R$ {balance:.2f}")
-        print("==========================================")
+        print("==================================================")
 
     elif option == "c":
         break
 
     else:
-        print("Invalid option, please choose again from the menu.")
+        print("Please select a valid option.")
